fb2806645e613380198079e9a9a5ae4fd7a9c5b2
Letjobana <suzanm@gerbertech.co.za>
2022-08-08 11:18:53 +0200

    Added BaseEmployeeList class

---
+++ BlazorWebApi/EmployeeManagement.Web/BaseEmployeeList.cs
@@ -0,0 +1,47 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web
+{
+    public class BaseEmployeeList : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployee();
+            return base.OnInitializedAsync();
+        }
+        private void LoadEmployee()
+        {
+            Employee e = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Suzan",
+                LastName = "Masite",
+                Email = "mahlodimasite@gmail.com",
+                DateOfBirth = new DateTime(1990 / 01 / 01),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 3, DepartmentName = "IT" }
+
+            };
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Smith",
+                Email = "john@gmail.com",
+                DateOfBirth = new DateTime(1990 / 01 / 03),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 1, DepartmentName = "HR" }
+
+            };
+            Employees = new List<Employee>
+            {
+                e,e1
+            };
+        }
+    }
+}
--- BlazorWebApi/EmploymentManagement.Models/Employee.cs
+++ BlazorWebApi/EmploymentManagement.Models/Employee.cs
@@ -5,7 +5,7 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public string FirstNameme { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
