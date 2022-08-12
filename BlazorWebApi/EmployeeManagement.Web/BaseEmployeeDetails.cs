818a3ebf729bcf47027bf3b4da7799c6bb0bd32b
Letjobana <suzanm@gerbertech.co.za>
2022-08-12 11:41:05 +0200

    Updated the BaseEmployeeDetails

--- BlazorWebApi/EmployeeManagement.Web/BaseEmployeeDetails.cs
+++ BlazorWebApi/EmployeeManagement.Web/BaseEmployeeDetails.cs
@@ -1,5 +1,7 @@
 ﻿using EmployeeManagement.Api.Repositories.Abstracts;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace EmployeeManagement.Web
 {
@@ -8,6 +10,12 @@ namespace EmployeeManagement.Web
         [Parameter]
         public string Id { get; set; }
         [Inject]
-        public  IEmployeeService EmployeeService { get; set; }
+        public IEmployeeService EmployeeService { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
     }
 }
